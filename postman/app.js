6c1fc5fb67cb8a2ba17aa8d203cf5fd5f509a934
tamarcy <cohen.tamar@gmail.com>
2022-01-12 15:47:42 +0200

    crud

--- postman/app.js
+++ postman/app.js
@@ -1,25 +1,35 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 
 const port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}`);
-})
+const numbersArray = [1, 2, 3, 4, 5, 6];
 
-
-
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
 
 // app.<http header>("<route>",(req,res)=>{
-//     <Callback function that specifies what we need to do when the sepcific route is accessed>
+//     <Callback function that specifies what we need to do when the specific route is accessed>
 //     });
 
-app.get("/numbers",(req,res)=> res.status(200).send({message: "success using get"}))
+// app.get("/numbers",(req,res)=> res.status(200).send({message: "success using get"}))
 
+app.get("/", (req, res) => res.send(numbersArray));
 
-// app.get("/",(req,res)=> res.send({message: "HELLO"}))
+app.post("/numbers/:num", (req, res) => {
+  const num = +req.params.num;
+  if (numbersArray.includes(num)) {
+    res.status(400).send(`${num} all ready exist`);
+  } else {
+    numbersArray.push(num);
+    res.send(numbersArray);
+  }
+});
 
-app.post("/numbers",(req,res)=> res.send("success using post"))
+// app.get('/users/:userId/books/:bookId', function (req, res) {
+//     res.send(req.params)
+//   })
 
 // app.post("/", (req, res) =>
 // res
@@ -27,6 +37,25 @@ app.post("/numbers",(req,res)=> res.send("success using post"))
 // .json({ message: "You can post to this endpoint !",
 // app:"Express-Routes"}));
 
-app.put("/numbers",(req,res)=> res.send("success using put"))
+app.put("/numbers", (req, res) => {
+  const {num1, num2} = req.query;
+  if (!numbersArray.includes(num1)){
+      res.status(400).send(`${num1} is not in the data`)
+  }
+//   const number2 = +req.query.num2;
+  const index = numbersArray.indexOf(num1);
+  numbersArray[index] = num2;
+  res.send(numbersArray);
+});
+// localhost:3000/numbers?num1=5&num2=9
 
-app.delete("/numbers",(req,res)=> res.send("success using delete"))
+app.delete("/numbers/:num", (req, res) => {
+  const num = +req.params.num;
+  if(!numbersArray.includes(num)){
+    res.status(400).send(`${num} is not in the data`);
+  } else {
+  const index = numbersArray.indexOf(num);
+  numbersArray.splice(index, 1);
+  res.send(numbersArray);
+  }
+});
